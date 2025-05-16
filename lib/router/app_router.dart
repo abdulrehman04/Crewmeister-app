2d636b5a49d8ebb9a4a6b90ff8b064d5b84355a7
abdulrehman04 <abdulrehman71254@gmail.com>
2025-05-16 15:25:58 +0200

    feat: Setup AbsenceManagerScreen structure and update routing

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:crewmeister_app/router/routes.dart';
+import 'package:crewmeister_app/screens/absence_manager/absence_manager.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
@@ -8,7 +9,7 @@ class AppRouter {
       GoRoute(
         path: AppRoutes.absenceManager,
         builder: (context, state) {
-          return const Placeholder();
+          return const AbsenceManagerScreen();
         },
       ),
     ],
