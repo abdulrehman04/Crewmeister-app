4551130e732e3af5ad4a0633e3266230bc2ab304
abdulrehman04 <abdulrehman71254@gmail.com>
2025-05-16 16:30:57 +0200

    feat: Add responsive design utilities with AppBreakpoints and AppMedia classes

---
+++ lib/configs/configs.dart
@@ -0,0 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+part 'ui/_breakpoints.dart';
+part 'ui/_media.dart';
---
+++ lib/services/responsive.dart
@@ -0,0 +1,38 @@
+import 'package:crewmeister_app/configs/configs.dart';
+import 'package:flutter/material.dart';
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget desktop;
+  const Responsive({
+    super.key,
+    required this.mobile,
+    required this.tablet,
+    required this.desktop,
+  });
+
+  // screen sizes
+  static bool isMobile() => AppMedia.width < AppBreakpoints.sm;
+
+  static bool isTablet() =>
+      AppMedia.width < AppBreakpoints.xmd &&
+      AppMedia.width >= AppBreakpoints.sm;
+
+  static bool isDesktop() => AppMedia.width >= AppBreakpoints.lg;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= AppBreakpoints.lg) {
+          return desktop;
+        } else if (constraints.maxWidth >= AppBreakpoints.md) {
+          return tablet;
+        } else {
+          return mobile;
+        }
+      },
+    );
+  }
+}
