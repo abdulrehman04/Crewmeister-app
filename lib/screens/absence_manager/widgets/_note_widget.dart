9276f3c0446333b9683b2d6d3da2df26a063604b
abdulrehman04 <abdulrehman71254@gmail.com>
2025-05-17 23:13:16 +0200

    feat: Add NoteWidget for displaying notes

---
+++ lib/screens/absence_manager/widgets/_note_widget.dart
@@ -0,0 +1,68 @@
+part of '../absence_manager.dart';
+
+class NoteWidget extends StatelessWidget {
+  final String title;
+  final String note;
+
+  const NoteWidget({super.key, required this.title, required this.note});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: AppTheme.kWhite,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 0),
+            color: const Color(0xff000000).withSafeOpacity(0.2),
+            blurRadius: 1,
+          ),
+        ],
+      ),
+      child: IntrinsicHeight(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: 6.w,
+              decoration: const BoxDecoration(
+                color: AppTheme.kPrimary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
+                ),
+              ),
+            ),
+            5.horizontalSpace,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 10.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16.sp,
+                      ),
+                    ),
+                    5.verticalSpace,
+                    Text(
+                      note,
+                      maxLines: 2,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
