09543b01dc2beaab8acba29757370b27cef4dc29
abdulrehman04 <abdulrehman71254@gmail.com>
2025-05-18 17:52:51 +0200

    fix: update ktextPrimary color value in AppTheme

--- lib/configs/theme/_colors.dart
+++ lib/configs/theme/_colors.dart
@@ -3,8 +3,9 @@ part of '../configs.dart';
 abstract class AppTheme {
   static const kPrimary = Color(0xFFfb9318);
   static const kSecondary = Color(0xffFFF2E0);
+  static const kBgColor = Color(0xffefe0d5);
   static const kBorderColor = Color(0xffFFE0B3);
   static const kWhite = Colors.white;
   static const ktextSecondary = Color.fromARGB(255, 137, 137, 139);
-  static const ktextPrimary = Color.fromARGB(255, 66, 66, 66);
+  static const ktextPrimary = Color.fromARGB(255, 97, 97, 97);
 }
