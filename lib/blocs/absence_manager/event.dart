79bccc0e5f17773fa997308f3d4e1662e25b7655
abdulrehman04 <abdulrehman71254@gmail.com>
2025-05-18 04:04:04 +0200

    feat: add FetchAbsenteesEvent with parameters for querying absentees

--- lib/blocs/absence_manager/event.dart
+++ lib/blocs/absence_manager/event.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
+
+import 'package:crewmeister_app/blocs/absence_manager/enums/absence_type.dart';
 
 class AbsenceManagerEvents extends Equatable {
   @override
@@ -6,6 +9,28 @@ class AbsenceManagerEvents extends Equatable {
 
   const AbsenceManagerEvents();
 }
+
+class FetchAbsenteesEvent extends AbsenceManagerEvents {
+  final String? query;
+  final AbsenceType? absenceType;
+  final DateTime? startDate;
+  final DateTime? endDate;
+  final String? status;
+  final int page;
+  final int pageSize;
+
+  const FetchAbsenteesEvent({
+    this.query,
+    this.absenceType,
+    this.startDate,
+    this.endDate,
+    this.status,
+    required this.page,
+    required this.pageSize,
+  });
+}
+
+class FetchAbsenteeCount extends AbsenceManagerEvents {}
 
 class AbsenceManagerTestEvent extends AbsenceManagerEvents {
   const AbsenceManagerTestEvent();
