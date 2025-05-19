4eb8461ae64983379ff9be8c04a8f7bab3b6bc73
abdulrehman04 <abdulrehman71254@gmail.com>
2025-05-19 14:50:50 +0200

    feat: created api service

---
+++ lib/services/api_service.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static final ApiService instance = ApiService._();
+  ApiService._() {
+    baseUrl = getBaseUrl();
+  }
+
+  static late String baseUrl;
+
+  getBaseUrl() {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080';
+    }
+    return 'http://localhost:8080';
+  }
+
+  Future<http.Response> get(String endpoint) async {
+    final url = Uri.parse('$baseUrl/$endpoint');
+    final response = await http.get(url);
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load data');
+    }
+    return response;
+  }
+}
